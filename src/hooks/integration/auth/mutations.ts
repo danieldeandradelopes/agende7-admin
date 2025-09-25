9cbae653290a2c713cb7bc4b0108c3f648f51f25
Daniel Lopes <danieldeandradelopes@gmail.com>
2025-09-25 11:11:00 -0300

    feat: add provider login

--- src/hooks/integration/auth/mutations.ts
+++ src/hooks/integration/auth/mutations.ts
@@ -12,48 +12,8 @@ export const useLogin = () => {
   return useMutation<Authentication, Error, LoginType>({
     mutationFn: async (value) => {
       const response = await api.post<Authentication>({
-        url: "/login",
+        url: "/login/superadmin",
         data: value,
-      });
-      login(
-        response.token,
-        response.user,
-        response.barbershop ?? defaultBarberShop
-      );
-
-      return response;
-    },
-    mutationKey: [AUTH_KEYS.login],
-    onError: (err) => {
-      console.log(err);
-      showNotification("Erro ao fazer login");
-    },
-    retry: false,
-  });
-};
-
-export interface ProviderAuthData {
-  email: string;
-  phone: string;
-  name: string;
-  avatar: string;
-  provider: string;
-  providerUid: string;
-  barberShopId?: number;
-  token: string;
-}
-
-export const useProviderLogin = () => {
-  const { login } = useAuth();
-
-  return useMutation<Authentication, Error, ProviderAuthData>({
-    mutationFn: async (value) => {
-      const response = await api.post<Authentication>({
-        url: "/provider/login",
-        data: value,
-        headers: {
-          Authorization: `Bearer ${value.token}`,
-        },
       });
       login(
         response.token,
